01b665d8ea4c79809c52e4fe03f765d6375d1aef
gpffh20 <eunbin584836@gmail.com>
2023-05-05 02:42:05 +0900

    session10

---
+++ serengeti/main/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from article.models import Article
+
+def index(request):
+    article = Article.objects.all()
+    return render(request, 'index.html', {'articles':articles})
